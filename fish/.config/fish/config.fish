c05448e048ca55c5444d120b94b97ca957d81961
David Villeneuve <david.villeneuve.4@gmail.com>
2026-05-16 22:36:35 +0200

    start of a config for starship

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# uv
+fish_add_path "/home/dvillene/.local/share/../bin"
+
+starship init fish | source

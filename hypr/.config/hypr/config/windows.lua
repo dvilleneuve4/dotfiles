5ce7f8378d75a44b15176fb6d555a4e61fc30c1e
David Villeneuve <david.villeneuve.4@gmail.com>
2026-05-18 20:33:49 +0200

    adding some more transparent windows

--- hypr/.config/hypr/config/windows.lua
+++ hypr/.config/hypr/config/windows.lua
@@ -7,6 +7,22 @@ hl.window_rule({
   
 })
 hl.window_rule({
+  name = "transparent-discord",
+  match = {
+    class = "discord"
+  },
+  opacity = 0.9,
+  
+})
+hl.window_rule({
+  name = "transparent-whatsapp",
+  match = {
+    class = "brave-web.whatsapp.com__-Default"
+  },
+  opacity = 0.9,
+  
+})
+hl.window_rule({
   name = "fullscreen-bypass-idle",
   match = {
     class = "brave-www.youtube.com__-Default"
